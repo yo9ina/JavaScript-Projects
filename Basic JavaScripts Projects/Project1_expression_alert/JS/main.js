1e6992238c9ed3e8d55ce68e0b4ddb7c5674865c
yo9ina <lucian@yo9ina.ro>
2022-11-29 19:53:40 +0000

    Assignment#11 with deprecated fontcolor

--- Basic JavaScripts Projects/Project1_expression_alert/JS/main.js
+++ Basic JavaScripts Projects/Project1_expression_alert/JS/main.js
@@ -2,5 +2,15 @@ var Family = "The Arezzinis",
     Dad = "Jeremiah",
     Mom = "Hermoine",
     Daughter = "Penny",
-    Son = "Zorro";
-document.write(Son);
+    Son = "Zorro",
+    Son = Son.fontcolor("blue"),
+    Daughter = Daughter.fontcolor("red"),
+    Mom = Mom.fontcolor("grey"),
+    Dad = Dad.fontcolor("brown"),
+    Family = Family.fontcolor("green");
+
+document.writeln(Son);
+document.writeln(Daughter);
+document.writeln(Mom);
+document.writeln(Dad);
+document.writeln(Family);
