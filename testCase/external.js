233743b15d98dfe0428aecd0fdda6065168c1cc7
yo9ina <lucian@yo9ina.ro>
2022-11-29 18:48:21 +0000

    Initial commit
    Created before this repository

---
+++ testCase/external.js
@@ -0,0 +1,14 @@
+var x = parseInt(prompt("Alege un numar intre 1 si 3"));
+switch(x) {
+    case 1:
+        alert("Ai ales numarul 1");
+        break;
+    case 2:
+        alert("Ai ales numarul 2");
+        break;
+    case 3:
+        alert("Ai ales numarul 3");
+        break;
+    default:
+        alert("Ai ales alt numar");
+}
---
+++ testFunctie/external.js
@@ -0,0 +1,7 @@
+function limonada(apa, lamai){
+    var instructiuni = "Pune " + apa + " ml apa intr-un pahar apoi stoarce " + lamai + " lamai"
+    return instructiuni
+}
+
+// limonada(200,2)
+alert(limonada(200,3));
