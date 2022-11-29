a10682712b5c29e137e1191feff71454b3dc9cdb
yo9ina <lucian@yo9ina.ro>
2022-11-29 20:20:31 +0000

    Assignment Submission # 1

--- Basic JavaScripts Projects/Project1_expression_alert/JS/main.js
+++ Basic JavaScripts Projects/Project1_expression_alert/JS/main.js
@@ -1,13 +1,13 @@
-var Family = "The Arezzinis",
+var Family = "The Arezzinis",           // Assign values to variable
     Dad = "Jeremiah",
     Mom = "Hermoine",
     Daughter = "Penny",
-    Son = "Zorro",
+    Son = "Zorro",                      
     Son = Son.fontcolor("blue"),
     Daughter = Daughter.fontcolor("red"),
     Mom = Mom.fontcolor("grey"),
     Dad = Dad.fontcolor("brown"),
-    Family = Family.fontcolor("green");
+    Family = Family.fontcolor("green"); // Assign a color to one variable using the fontcolor() method.
 
 document.writeln(Son);
 document.writeln(Daughter);
@@ -15,4 +15,5 @@ document.writeln(Mom);
 document.writeln(Dad);
 document.writeln(Family);
 
-document.write(99 / 3);
+document.write(99 / 3); // Write an expression.
+document.writeln(Family + " " + Son);     // concatenate the variables together
