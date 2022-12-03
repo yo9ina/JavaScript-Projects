a3e4cc02fc84b83258aa49b1b2bc2bd2663f470f
yo9ina <lucian@yo9ina.ro>
2022-12-03 04:44:38 +0000

    Challenge #5

--- Basic JavaScripts Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScripts Projects/Project4_dictionaries/JS/main.js
@@ -2,9 +2,10 @@ function funcDictionary() {
     var France = {
         Name: "Republique Francaise",
         Capital: "Paris",
+        Capital: "Parris",
         Language: "French",
         Population: "67,897,000 (July 2022, estimate)",
         Currency: "Euro"
     };
-    document.getElementById("Dictionary").innerHTML = France.Population;
+    document.getElementById("Dictionary").innerHTML = France.Capital;
 }
