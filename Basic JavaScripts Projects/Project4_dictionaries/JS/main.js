8a100c511f741dc8119935bc2507745291c1acf9
yo9ina <lucian@yo9ina.ro>
2022-12-03 04:27:47 +0000

    Assignment #24

---
+++ Basic JavaScripts Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,10 @@
+function funcDictionary() {
+    var France = {
+        Name: "Republique Francaise",
+        Capital: "Paris",
+        Language: "French",
+        Population: "67,897,000 (July 2022, estimate)",
+        Currency: "Euro"
+    };
+    document.getElementById("Dictionary").innerHTML = France.Population;
+}
