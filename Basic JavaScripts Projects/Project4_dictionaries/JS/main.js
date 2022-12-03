e711fbc41082a3fe708fa2c4735573daafa73ac5
yo9ina <lucian@yo9ina.ro>
2022-12-03 05:00:12 +0000

    Assignment Submission #4

--- Basic JavaScripts Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScripts Projects/Project4_dictionaries/JS/main.js
@@ -6,6 +6,7 @@ function funcDictionary() {
         Population: "67,897,000 (July 2022, estimate)",
         Currency: "Euro"
     };
-    delete France.Population;
-    document.getElementById("Dictionary").innerHTML = France.Population;
+    delete France.Population; // delete record from Population
+    document.getElementById("Dictionary").innerHTML = France.Population;    // display "undefined" if nothing is follow
+    document.getElementById("Dictionary").innerHTML = France.Name;          // display "Republique Francaise"
 }
