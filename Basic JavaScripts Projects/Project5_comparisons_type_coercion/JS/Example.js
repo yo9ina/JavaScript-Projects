6cce6f8bfb975b1a5983eefcd84471bfe3ef1c5f
yo9ina <lucian@yo9ina.ro>
2022-12-12 23:04:48 +0000

    Assignment #27

--- Basic JavaScripts Projects/Project5_comparisons_type_coercion/JS/Example.js
+++ Basic JavaScripts Projects/Project5_comparisons_type_coercion/JS/Example.js
@@ -1,3 +1,5 @@
 function myFunction() {
     document.getElementById("Test").innerHTML = isNaN('007');
 }
+
+document.write(2E310);
---
+++ Basic JavaScripts Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,6 @@
+function myFunctionPos() {
+    document.getElementById("Test1").innerHTML = 2E310;
+}
+function myFunctionNeg() {
+    document.getElementById("Test2").innerHTML = -3E310;
+}
