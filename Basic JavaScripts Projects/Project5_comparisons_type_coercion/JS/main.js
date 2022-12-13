9f7f0badb81abfa1b569ae3dfcd802a991bf864d
yo9ina <lucian@yo9ina.ro>
2022-12-13 00:49:21 +0000

    Assignment #30

--- Basic JavaScripts Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScripts Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,7 +1,4 @@
-function myFunctionPos() {
-    document.getElementById("Test1").innerHTML = 10 > 3;
-}
-function myFunctionNeg() {
-    document.getElementById("Test2").innerHTML = 10 <= 3;
-    console.log(2 + 2);
+
+function myFunction() {
+    document.write('10' + 5);
 }
