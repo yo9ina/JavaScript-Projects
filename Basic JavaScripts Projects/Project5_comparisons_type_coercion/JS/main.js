3a2923b5a70ae305a99997ad71499433d19e0e07
yo9ina <lucian@yo9ina.ro>
2022-12-13 05:31:47 +0000

    Assignment #34

--- Basic JavaScripts Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScripts Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,13 +1,5 @@
-function funcTrEq(){
-    var a = 15,
-        b = 20,
-        c = 30,
-        d = 10,
-        e = 40,
-        f = 11,
-        m = document.getElementById("tripleEqual").innerHTML = ((a > d) + " AND " + (a < c) + " = " + ((a>d)&&(a<c))),
-        n = document.getElementById("tripleEqual").innerHTML = ((a < d) + " AND " + (a < c) + " = " + ((a<d)&&(a<c))),
-        o = document.getElementById("tripleEqual").innerHTML = ((b > d) + " OR " + (e < f) + " = " + ((a>d)||(a<c)));
-        p = document.getElementById("tripleEqual").innerHTML = ((a > d) + " OR " + (a < c) + " = " + ((a>d)||(a<c)));
-    document.write(m + "<br>" + n + "<br>" + o + "<br>" + p);
+function funcNot(){
+    var a = document.getElementById("not").innerHTML = !(5 > 10);    // true
+    var b = document.getElementById("not").innerHTML = !(5 < 10);    // false
+    document.write("Negation for (5 > 10) is " + a + "<br>Negation for (5 < 10) is " + b);
 }
