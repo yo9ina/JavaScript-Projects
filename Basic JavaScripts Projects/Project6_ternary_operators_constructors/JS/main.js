fee213a118649c6e8417d69b0b7ec28dd2de7589
yo9ina <lucian@yo9ina.ro>
2022-12-25 07:32:49 +0000

    Assignment #38

--- Basic JavaScripts Projects/Project6_ternary_operators_constructors/JS/main.js
+++ Basic JavaScripts Projects/Project6_ternary_operators_constructors/JS/main.js
@@ -1,15 +1,8 @@
-function Vehicle(Make, Model, Year, Color) {
-    this.Vehicle_Make = Make;
-    this.Vehicle_Model = Model;
-    this.Vehicle_Year = Year;
-    this.Vehicle_Color = Color;
-}
-var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
-var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
-var Erik =new Vehicle("Ford", "Pinto", 1971, "Mustard");
-
 function myFunction() {
-    document.getElementById("New_and_This").innerHTML = 
-    "Jack drives a " + Jack.Vehicle_Color + "-colored " + Jack.Vehicle_Model +
-    " manufactured in " + Jack.Vehicle_Year;
+    document.getElementById("Nested_Function").innerHTML = azi();
+    function azi() {
+        const z = new Date();
+        let timp = z.toTimeString();
+        return timp;
+    }
 }
