3f6d5cba970cab6f56db6d33e5218e1e3be261dc
yo9ina <lucian@yo9ina.ro>
2022-12-25 03:15:35 +0000

    Assignment Submission #5

--- Basic JavaScripts Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScripts Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,5 +1,30 @@
-function funcNot(){
+// Demo for NOT using document.write() method
+function funcNot() {
     var a = document.getElementById("not").innerHTML = !(5 > 10);    // true
     var b = document.getElementById("not").innerHTML = !(5 < 10);    // false
     document.write("Negation for (5 > 10) is " + a + "<br>Negation for (5 < 10) is " + b);
+}
+
+// String and number combination
+function funcStrNum() {
+  //  document.write('10' + 5);
+    var x = ("10" + 5) < 200;
+    var y = ("10" + 5) > 200;
+    document.getElementById("str_num").innerHTML= ("<p>((\"10\" + 5) < 200) = " + x + "</p><p>((\"10\" + 5) > 200) = " + y + "</p>");
+
+}
+
+// Function which use few operators
+function funcOper() {
+    var vOr =  ("3 == 3 " + (3==3)) || ("3 == 4 " + (3==4)),
+        vAnd = ("3 === 4 " + (3===4)) && ("3 === 5 " + (3===5)),
+        vGreat = ("3 > 2 = " + (3>2)),
+        vLess = ("3 < 2 = " + (3<2)),
+        vNot = ("3 < 2 = " + !(3<2));
+    document.getElementById("oper").innerHTML = 
+        "OR &nbsp; &nbsp;" + vOr + "<br>" + 
+        "AND &nbsp; &nbsp;" + vAnd + "<br>" +
+        "> &nbsp; &nbsp;" + vGreat + "<br>" +
+        "< &nbsp; &nbsp;" + vLess + "<br>" +
+        "! &nbsp; &nbsp;" + vNot;
 }
