7fec16a390096b4aea8086d13a41cfc0321d1340
yo9ina <lucian@yo9ina.ro>
2023-01-09 15:30:46 +0000

    Assignment #43

--- Basic JavaScripts Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScripts Projects/Project7_scope_time_function/JS/main.js
@@ -1,12 +1,15 @@
-function myFunction_2() {
-    // Waiting for a number and is add to x variable
-    let x = document.getElementById("yourNumber").value;
-
-    if ((x < 220) || (x > 220)) {
-        text = "Error: <br>Your number is not 220."; // If x is not 220 then "text" get a new (string) value;
-    } else {
-        text = "Correct: <br>Your number is 220."   // If x is 220 then "text" get a "Confirmation" message;
+function Time_function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning time."
+    }
+    else if (Time > 12 == Time < 18) {
+        Reply = "It is the afternoon."
+    }
+    else {
+        Reply = "It is evening time."
     }
 
-    document.getElementById("test").innerHTML = text;
+    document.getElementById("Time_of_day").innerHTML = Reply;
 }
