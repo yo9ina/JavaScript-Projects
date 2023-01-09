9a8350e59ddacfcde938228005da2bf274b48b7f
yo9ina <lucian@yo9ina.ro>
2023-01-09 14:49:18 +0000

    Assignment #41

--- Basic JavaScripts Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScripts Projects/Project7_scope_time_function/JS/main.js
@@ -1,15 +1,14 @@
-/* 
-    1. Write out a function with an if statement that utilizes
-        the new Date().getHours() method covered in the previous course step.
-    2. Write out a document.getElementById() method as covered
-        in the previous course step and reference the id attribute
-        in the HTML file.
-    3. Save the file.
-*/
+function myFunction_2() {
+    // Waiting for a number and is add to x variable
+    let x = document.getElementById("yourNumber").value;
 
-function myFunction() {
-    var timp = new Date().getHours();
-    if (timp < 18) {
-        document.getElementById("test").innerHTML = "How are you today?";
+    // Initialize var "text" 
+    let text = "Your number is bigger or equal with 220.";
+
+
+    if (x < 220) {   // If x<10 then "text" get a new (string) value
+        text = "Your number is lower then 220.";
     }
+
+    document.getElementById("test").innerHTML = text;
 }
