f8aa2be46a8167d431dcc62888a3bc87256775b7
yo9ina <lucian@yo9ina.ro>
2023-01-09 15:17:44 +0000

    Assignment #42

--- Basic JavaScripts Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScripts Projects/Project7_scope_time_function/JS/main.js
@@ -2,12 +2,10 @@ function myFunction_2() {
     // Waiting for a number and is add to x variable
     let x = document.getElementById("yourNumber").value;
 
-    // Initialize var "text" 
-    let text = "Your number is bigger or equal with 220.";
-
-
-    if (x < 220) {   // If x<10 then "text" get a new (string) value
-        text = "Your number is lower then 220.";
+    if ((x < 220) || (x > 220)) {
+        text = "Error: <br>Your number is not 220."; // If x is not 220 then "text" get a new (string) value;
+    } else {
+        text = "Correct: <br>Your number is 220."   // If x is 220 then "text" get a "Confirmation" message;
     }
 
     document.getElementById("test").innerHTML = text;
