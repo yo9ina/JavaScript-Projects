82b586e3624a0eb9552191c7074d80bdee3a96b9
yo9ina <lucian@yo9ina.ro>
2023-01-16 13:33:07 +0000

    Assignment Submission #7

--- Basic JavaScripts Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScripts Projects/Project7_scope_time_function/JS/main.js
@@ -1,15 +1,31 @@
+var Reply;  // global variable
+
 function Time_function() {
-    var Time = new Date().getHours();
-    var Reply;
+    var Time = new Date().getHours();  // local variable
+
     if (Time < 12 == Time > 0) {
         Reply = "It is morning time."
     }
-    else if (Time > 12 == Time < 18) {
+    else if (Time > 11 == Time < 18) {
         Reply = "It is the afternoon."
     }
     else {
         Reply = "It is evening time."
     }
 
-    document.getElementById("Time_of_day").innerHTML = Reply;
+    document.getElementById("Time_of_day").innerHTML = Reply + " [" + Time + "]";
+}
+
+function errorFunc() {
+    var Time = new Date().getHours();
+
+    if (Time << 12) {  // error is here "<<"
+        Reply = "Time < 12"
+    }
+    else {
+        Reply = "Time > 12"
+    }
+
+    document.getElementById("errFunc").innerHTML = Reply; // display false result
+    console.log(Reply);                                   // display correct result in console
 }
