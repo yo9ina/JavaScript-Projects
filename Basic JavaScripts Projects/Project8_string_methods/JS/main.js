a0b89672a1ef36320b661a2a9fdc0b991765fd96
yo9ina <lucian@yo9ina.ro>
2023-02-20 03:11:56 +0000

    Assignment #45

--- Basic JavaScripts Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScripts Projects/Project8_string_methods/JS/main.js
@@ -1,3 +1,4 @@
+// Assignment #44
 // Create a JS folder within the Project8_string_methods folder.  
 // Create a new file within the JS folder and name it main.js.
 // In it, utilize the concat() method to connect two or more strings.
@@ -9,4 +10,15 @@ function full_Sentence() {
     var part_4 = "sentence.";
     var whole_sentence = part_1.concat(part_2, part_3, part_4);
     document.getElementById("Concatenate").innerHTML = whole_sentence;
+}
+
+
+// Assignment #45
+// Working within the Project8_string_methods folder:
+// Within your main.js file, write a slice() method that displays a section of a string.
+
+function slice_Method() {
+    var Sentence = "My name is Lucian and I will show you this on the screen.";
+    var Section = Sentence.slice(11, 17);
+    document.getElementById("Slice").innerHTML = Section;
 }
