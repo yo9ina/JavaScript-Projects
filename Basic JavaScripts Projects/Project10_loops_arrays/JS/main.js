b3d9f5105809cbdc630b57002c692f3a05ed40bc
yo9ina <lucian@yo9ina.ro>
2023-02-26 21:46:35 +0000

    Assignment #52

--- Basic JavaScripts Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScripts Projects/Project10_loops_arrays/JS/main.js
@@ -1,13 +1,17 @@
 
-// Assignment #51
+// Assignment #52
 
-var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
-var Content = "";
-var Y;
+// Open the main.js file and write an array within a function to be called.
+// Utilize a function with the document.getElementById().innerHTML method
+// (that references the id attribute in your HTML code) to display a value (or values) of your array.
+// Save the file.
+// Successfully execute index.html in the browser.
 
-function forLoop() {
-    for (Y = 0; Y < Instruments.length; Y++) {
-        Content += "Instrument #" + (Y + 1) + " is: " + Instruments[Y] + "<br>";
-    }
-    document.getElementById("List_of_instruments").innerHTML = Content;
+function arrayFunc() {
+    var me = [];
+    me[0] = "working";
+    me[1] = "reading";
+    me[2] = "running";
+    me[3] = "playing";
+    document.getElementById("Array").innerHTML = "In this picture, I am " + me[3] + ".";
 }
