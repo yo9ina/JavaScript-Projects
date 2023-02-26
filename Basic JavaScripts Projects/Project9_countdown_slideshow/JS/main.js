8597bccea4a1bfecb29a8dc5fb22d6431cfacca6
yo9ina <lucian@yo9ina.ro>
2023-02-26 20:37:43 +0000

    Assignment Submission #9 v2

--- Basic JavaScripts Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic JavaScripts Projects/Project9_countdown_slideshow/JS/main.js
@@ -3,15 +3,28 @@
 function countdown() {
     var seconds = document.getElementById("seconds").value;
 
+    // Is working but counter is not stoping after mesage "Time's up!" if you click on it
+    //    function tick() {
+    //        seconds = seconds - 1;
+    //        timer.innerHTML = seconds;
+    //        setTimeout(tick, 1000);
+    //        if (seconds == -1) {
+    //            alert("Time's up!");
+    //        }
+    //    }
+    //    tick();
+
+
+    // This is working.
     function tick() {
         seconds = seconds - 1;
         timer.innerHTML = seconds;
-        setTimeout(tick, 1000);
-        if (seconds == -1) {
-            alert("Time's up!");
+        if (seconds > 0) {
+            setTimeout(tick, 1000);
+        } else {
+            alert("Time's up!")
         }
     }
     tick();
 }
 
-// Is working but counter is not stoping after mesage "Time's up!" if you click on it
