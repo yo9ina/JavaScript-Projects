91800fac8183473469a6fe963f0800a12d36885e
yo9ina <lucian@yo9ina.ro>
2023-02-27 02:30:09 +0000

    Assignment #53

--- Basic JavaScripts Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScripts Projects/Project10_loops_arrays/JS/main.js
@@ -1,17 +1,25 @@
 
-// Assignment #52
+// Assignment #53
 
-// Open the main.js file and write an array within a function to be called.
-// Utilize a function with the document.getElementById().innerHTML method
-// (that references the id attribute in your HTML code) to display a value (or values) of your array.
+// Create an object through utilization of the const keyword.
+// Include properties with values.
+// Create a function (constant_function()) to be called from your HTML
+// that will display a string using the
+// document.getElementById("Constant").innerHTML method
+// that includes a property value (or property values) of your object.
+// Successfully execute index.html in the browser.
+
+// Return to the main.js and take the following actions:
+// Change a property’s value.
+// Add a property with a value.
+// Display a string that includes the added property value
+// and the changed property value using document.getElementById("Constant").innerHTML method.
 // Save the file.
 // Successfully execute index.html in the browser.
 
-function arrayFunc() {
-    var me = [];
-    me[0] = "working";
-    me[1] = "reading";
-    me[2] = "running";
-    me[3] = "playing";
-    document.getElementById("Array").innerHTML = "In this picture, I am " + me[3] + ".";
+function constant_function() {
+    const dog = { gender: "male", age: 3, color: "brown" };
+    dog.age = 5;
+    // dog.color = "white";
+    document.getElementById("Constant").innerHTML = "Rex is an " + dog.color + " " + dog.gender + ", " + dog.age + " years old.";
 }
