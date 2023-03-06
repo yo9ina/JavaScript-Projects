4be67102db3a7cf3018363679c7bff8db5d517cb
yo9ina <lucian@yo9ina.ro>
2023-03-06 06:11:17 +0000

    Challenge #16

--- Basic JavaScripts Projects/JavaScript/JS/JavaSscript_1.js
+++ Basic JavaScripts Projects/JavaScript/JS/JavaSscript_1.js
@@ -7,9 +7,26 @@ function hiFunc() {
     A[0].innerHTML = "The text has changed.";
 }
 
+// ===============================================
+
 // Assignment #58
 // Within the HTML_1.html file, do the following:
 // Use the <canvas> element
 // Add a border to the canvas
 // Save your code.
 // Successfully execute HTML_1.html in the browser.
+// ================================================
+
+// Challenge #16
+// Research how JavaScript can be utilized with the <canvas> element to create graphics.
+// Within the JavaScript_1.js file utilize JavaScript to add a graphic within the canvas.
+// Save your code and successfully execute it in the browser.
+
+function canVas() {
+    let canvas = document.getElementById("canvas");
+    let ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = "#f00";
+    ctx.fillRect(2, 2, 296, 146);
+    ctx.stroke();
+}
