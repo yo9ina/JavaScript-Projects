3db3e63194cc6444778aaf3604d25062f4e4a1dd
yo9ina <lucian@yo9ina.ro>
2023-03-06 05:27:47 +0000

    Assignment #58

--- Basic JavaScripts Projects/JavaScript/JS/JavaSscript_1.js
+++ Basic JavaScripts Projects/JavaScript/JS/JavaSscript_1.js
@@ -6,3 +6,10 @@ function hiFunc() {
     var A = document.getElementsByClassName("click");
     A[0].innerHTML = "The text has changed.";
 }
+
+// Assignment #58
+// Within the HTML_1.html file, do the following:
+// Use the <canvas> element
+// Add a border to the canvas
+// Save your code.
+// Successfully execute HTML_1.html in the browser.
